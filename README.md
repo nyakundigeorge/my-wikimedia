50dacef40953753ff79a53b30c65bf490ef38c3a
nyakundigeorge <gee.nyakundi@gmail.com>
2020-02-07 22:55:25 +0300

    feat(final-draft): all pieces tied in together

---
+++ README.md
@@ -0,0 +1,32 @@
+# Wikimedia Coding Test
+
+## Task:
+Develop a webpage with a form that allows a user to enter a Wikipedia article name and
+choose the language version of Wikipedia. When the form is submitted, fetch the table of
+contents of the article and display it in hierarchy. Clicking on an item in table of contents
+takes the user to that section in the corresponding Wikipedia article – in a new browser tab.
+An example of the user interface is given below.
+
+## Getting Started
+
+Git clone this repository, and serve the application and access the index.html file on your browser
+
+### Prerequisites
+Needs an active internet connection
+## Running the tests
+
+No tests have been defined in this application
+
+
+## Built With
+
+* [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+* [CSS3](http://www.css3.info/)
+* [JS](https://javascript.info/)
+
+
+## Authors
+
+* **George Nyakundi** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
--- js/index.js
+++ js/index.js
@@ -1,25 +1,110 @@
 /**
  * Author: George Nyakundi
- *  Handles all network requests
+ *
  */
 
- // returns an latest html of the page.
 /**
  * Language codes
  * en - English
  * es - Spanish
  * vi - Vietnamese
  */
- 
 
- let wikipedia_latest_html_url = 'https://en.wikipedia.org/api/rest_v1/page/html/';
- let wikipedia_metadata_html_url = 'https://en.wikipedia.org/api/rest_v1/page/metadata/'
+ //
+ document.getElementById("search").addEventListener("submit",handleSubmit,false);
 
- //example request: https://en.wikipedia.org/api/rest_v1/page/metadata/Daniel Moi
+ let isLoading = false;
 
 
- async function fetchMetadata(e){
-     e.preventDefault()
-     alert("Fetch Button clicked")
-    // return await fetch();
+ async function handleSubmit(e){
+    document.getElementById('submitButton').disabled = true;
+     e.preventDefault();
+     let metadataRes = await fetchInfo('metadata');
+     let wikiRes  = await fetchInfo('summary')
+     let wikiUrl = ''
+
+     if(wikiRes && wikiRes.title !="Not found." ){
+        wikiUrl = getWikiUrls(wikiRes);
+    }else{
+       document.getElementById("container-main-content").innerHTML = 'Document Not Found'
+       document.getElementById('submitButton').disabled =  false
+       return
+    }
+
+     if(metadataRes && !metadataRes.title){
+         //generate table of contents
+        generateTOC(metadataRes,wikiUrl)
+        document.getElementById('submitButton').disabled =  false
+     }else{
+         document.getElementById("container-content-navigation").innerHTML = 'Document Not Found'
+         document.getElementById('submitButton').disabled =  false
+         return
+     }
  }
+
+ /**
+  * Fetches a particular resource from the api
+  * @param {*} requestOption
+  */
+ async function fetchInfo(requestOption){
+     let articleName = document.getElementById("articleName").value;
+     let lang = document.getElementById("lang").value;
+     if(!requestOption){
+         return
+     }
+     if(articleName && articleName.trim().length > 3){
+        const response = await fetch(`https://${lang}.wikipedia.org/api/rest_v1/page/${requestOption}/${articleName}`,
+        {
+            mode:'cors',
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        const results = await response.json()
+        return results
+
+     }else{
+         return {"title": "Not found."}
+     }
+ }
+
+/**
+ * Function to fetch the wiki urls for a particular article
+ * @param {*} resultsObj
+ */
+  function getWikiUrls(resultsObj){
+    if(resultsObj && resultsObj.title !='Not found.'){
+
+        if(isMobile()){
+            let {content_urls: {mobile:{page}}} = resultsObj;
+            return page
+        }else{
+            let {content_urls: {desktop:{page}}} = resultsObj;
+            return page
+        }
+    }else{
+        document.getElementById("container-content-navigation-toc").innerHTML = 'Summary Not Found'
+    }
+ }
+
+ async function generateTOC(resultsObj,wikiUrl){
+    let {toc : {title, entries }} = resultsObj;
+    let tocList = entries.map(entry => {
+        let tabspaces = ''
+        if(entry.level > 1){
+            let tab = "&ensp;"
+             tabspaces = tab.repeat(parseInt(entry.level));
+        }
+        return `<a href='${wikiUrl}#${entry.anchor}' target='_blank'><li>${tabspaces}${entry.level}.${entry.section}. ${entry.html}</li></a>`
+    }).join("")
+    document.getElementById("container-content-navigation-toc").innerHTML = tocList;
+    document.getElementById("container-content-navigation-title").innerHTML = title;
+ }
+
+/**
+ * Utility function to check the viewport size and fetch resource accordingly
+ */
+
+function isMobile(){
+    return window.matchMedia("(max-width: 700px)").matches
+}
